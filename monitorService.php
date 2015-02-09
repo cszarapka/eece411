06818eb1588685923a8f219764184090d68a6a87
zeromadcowz <rclarke.ce@gmail.com>
2015-02-09 01:40:07 -0800

    everything up to the end of the night

--- monitorService.php
+++ monitorService.php
@@ -4,7 +4,7 @@ $hostname = trim(shell_exec('hostname'));
 $nodesText = file_get_contents('nodes.txt');
 //DELETE NODES.TXT
 //CREATE RESULTS.TXT
-$resultFile = file_put_contents('results.txt', $hostname."|2|");
+$resultFile = file_put_contents('results'.$hostname.'.txt', $hostname."|2|");
 $diskUsed = trim(shell_exec('du -s -h'));
 $diskUsed = trim(strstr($diskUsed, ".", TRUE));
 
@@ -14,7 +14,7 @@ $diskAvail = trim(strstr(trim(strstr(trim(strstr( $diskAvail, "\n" ))," ")), " "
 $upTime = trim(shell_exec('uptime'));
 //echo $upTime."\n";
 
-file_put_contents('results.txt', $diskUsed.'|'.$diskAvail.'|'.$upTime."\n" , FILE_APPEND);
+file_put_contents('results'.$hostname.'.txt', $diskUsed.'|'.$diskAvail.'|'.$upTime."\n" , FILE_APPEND);
 
 //echo $nodesText."\n";
 
@@ -37,33 +37,34 @@ foreach( $nodesArray as $key => $slice )
 	if( ping($slice) )
 	{
 		echo 'Yes: '.$slice."\n\n";
-		$command = 'ssh -o StrictHostKeyChecking=no -l ubc_eece411_5 -i ~/.ssh/id_rsa '.$slice.' "sudo yum install php -y"';
+		$command = 'ssh -o StrictHostKeyChecking=no -l ubc_eece411_5 -i id_rsa '.$slice.' "sudo yum install php -y"';
 		$result = shell_exec($command." 2>&1");
 		echo 'Result: '.$result."\n";
 		echo stristr($result, 'Setting up');
 		if( $ping1 == '' and stristr($result, 'Setting up') != false)
 		{
 			echo 'Found Server';
-
 			$ping1 = $slice;
+			file_put_contents("sendingTo.txt",$ping1);
 			//WRITE $SLICE TO RESULTS.TXT + ONLINE
 		} 
 		else 
 		{
-			file_put_contents( 'results.txt', $slice."|1\n" , FILE_APPEND);
+			file_put_contents('results'.$hostname.'.txt', $slice."|1\n" , FILE_APPEND);
 		}
 
 		
-		if( $ping1 != '' && $ping2 == ''  and stristr($result, 'Setting up') != false)
+		if( $ping1 != '' && $ping1 != $slice && $ping2 == ''  and stristr($result, 'Setting up') != false)
 		{
 			echo 'Found Server';
 			$ping2 = $slice;
+			file_put_contents("sendingTo.txt",$ping2, FILE_APPEND);
 			//WRITE $SLICE TO RESULTS.TXT + ONLINE
 			break;
 		}
 		elseif($ping1 != '') 
 		{
-			file_put_contents( 'results.txt', $slice."|1\n" , FILE_APPEND);
+			file_put_contents( 'results'.$hostname.'.txt', $slice."|1\n" , FILE_APPEND);
 		}
 		/*
 		echo 'Yes: '.$slice."\n\n";
@@ -75,7 +76,7 @@ foreach( $nodesArray as $key => $slice )
 	else
 	{
 		echo 'No: '.$slice."\n\n";
-		file_put_contents( 'results.txt', $slice."|0\n", FILE_APPEND );
+		file_put_contents( 'results'.$hostname.'.txt', $slice."|0\n", FILE_APPEND );
 		//WRITE $SLICE TO RESULTS.TXT + OFFLINE
 
 	}
@@ -98,33 +99,39 @@ print_r($nodesArray);
 print_r($array1);
 print_r($array2);
 //for( $i = )
-
-$array1String = trim( implode("\n", $array1) );
-echo $array1String;
-file_put_contents( 'nodes.txt', $array1String );
-//upload ssh shit
-echo "\n";
-$command = 'scp -i ~/.ssh/id_rsa \'nodes.txt\' id_rsa id_rsa.pub monitorService.php ubc_eece411_5@'.$ping1.':';
-echo $command."\n";
-system($command);
-$command = 'ssh -o StrictHostKeyChecking=no -l ubc_eece411_5 -i ~/.ssh/id_rsa '.$ping1.' "php monitorService.php > /dev/null 2>&1 & && exit"';
-system($command);
-
+if($ping1 != '') {
+	file_put_contents("sendingTo.txt",$ping1, FILE_APPEND);
+	$array1String = trim( implode("\n", $array1) );
+	echo $array1String;
+	file_put_contents( 'nodes.txt', $array1String );
+	//upload ssh shit
+	echo "\n";
+	$command = 'scp -i id_rsa \'nodes.txt\' id_rsa id_rsa.pub monitorService.php ubc_eece411_5@'.$ping1.':';
+	echo $command."\n";
+	system($command);
+	$command = 'ssh -o StrictHostKeyChecking=no -l ubc_eece411_5 -i ~/.ssh/id_rsa '.$ping1.' "sudo php monitorService.php > /dev/null 2>&1 &"';
+	system($command);
+}
 echo "\n\n\n";
 
 
+if($ping2 != '') {
+	file_put_contents("sendingTo.txt", "\n".$ping2, FILE_APPEND);
+	$array2String = trim( implode("\n", $array2) );
+	file_put_contents( 
+		'nodes.txt', $array2String );
+	echo $array2String;
+	$command = 'scp -i id_rsa \'nodes.txt\' id_rsa id_rsa.pub monitorService.php ubc_eece411_5@'.$ping2.':';
+	echo $command."\n";
+	system($command);
+	$command = 'ssh -o StrictHostKeyChecking=no -l ubc_eece411_5 -i id_rsa '.$ping2.' "sudo php monitorService.php > /dev/null 2>&1 &"';
+	system($command);
+	//file_put_contents( 'nodes.txt', $array2 );
+	//upload ssh shit
+}
 
-$array2String = trim( implode("\n", $array2) );
-file_put_contents( 'nodes.txt', $array2String );
-echo $array2String;
-$command = 'scp -i ~/.ssh/id_rsa \'nodes.txt\' id_rsa id_rsa.pub monitorService.php ubc_eece411_5@'.$ping2.':';
-echo $command."\n";
+$command = 'scp -i id_rsa results'.$hostname.'.txt ubc_eece411_5@planetlab2.cs.ubc.ca:';
 system($command);
-$command = 'ssh -o StrictHostKeyChecking=no -l ubc_eece411_5 -i ~/.ssh/id_rsa '.$ping2.' "php monitorService.php > /dev/null 2>&1 & && exit"';
-system($command);
-//file_put_contents( 'nodes.txt', $array2 );
-//upload ssh shit
-
 
 
 //$command = 'ssh -o StrictHostKeyChecking=no -l ubc_eece411_5 -i ~/.ssh/id_rsa '.$slice.' "sudo yum install php -y"';
