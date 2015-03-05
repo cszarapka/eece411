21618b32e1d7a77ef45c30764e0c23821f1a0913
Ryan Clarke <rclarke.ce@gmail.com>
2015-03-05 15:57:12 -0800

    created node class

--- DistributedHashTable/src/com/group11/eece411/A4/Node.java
+++ DistributedHashTable/src/com/group11/eece411/A4/Node.java
@@ -1,5 +1,26 @@
 package com.group11.eece411.A4;
 
 public class Node {
-
+	private String IP;
+	private int port;
+	private int nodeNumber;
+	
+	public Node(String IP, int port, int nodeNumber){
+		this.IP = IP;
+		this.port = port;
+		this.nodeNumber = nodeNumber;
+	}
+	
+	public String getIP(){
+		return this.IP;
+	}
+	
+	public int getPort(){
+		return this.port;
+	}
+	
+	public int getNodeNum(){
+		return this.nodeNumber;
+	}
+	
 }
