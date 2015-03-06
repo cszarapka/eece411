11a7686f9e96da77651c47f67816091d41b5729d
g17-sb <redhot_bouthot@hotmail.com>
2015-03-05 16:07:31 -0800

    gitignore made

--- DistributedHashTable/src/com/group11/eece411/A4/Main.java
+++ DistributedHashTable/src/com/group11/eece411/A4/Main.java
@@ -1,5 +1,38 @@
 package com.group11.eece411.A4;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Main {
 
+	private static final boolean VERBOSE = false;
+
+
+
+
+	public static void main(String [] args) throws IOException {
+		joinTable();
+		DatagramSocket serverSocket = new DatagramSocket(4003);
+		byte[] receiveData = new byte[15500];
+        byte[] sendData = new byte[15500];
+        while(true)
+        {
+           DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+           serverSocket.receive(receivePacket);
+           if(VERBOSE) {
+	           String sentence = new String( receivePacket.getData());
+	           System.out.println("RECEIVED: " + sentence);
+           }
+        }
+	}
+	
+	
+	
+	
+	private static void joinTable() {
+		
+	}
+	
 }
