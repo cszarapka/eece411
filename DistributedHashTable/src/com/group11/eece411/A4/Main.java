82c8e6747851a2c0a467a1e820194f6dae3acc18
Ryan Clarke <rclarke.ce@gmail.com>
2015-03-05 20:44:34 -0800

    added messageformatter

--- DistributedHashTable/src/com/group11/eece411/A4/Main.java
+++ DistributedHashTable/src/com/group11/eece411/A4/Main.java
@@ -1,5 +1,5 @@
 package com.group11.eece411.A4;
 
 public class Main {
-
+	
 }
--- DistributedHashTable/src/com/group11/eece411/A4/Node.java
+++ DistributedHashTable/src/com/group11/eece411/A4/Node.java
@@ -1,20 +1,29 @@
 package com.group11.eece411.A4;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /*
  * Node data structure
  */
 public class Node {
-	private String IP;
+	private byte[] IP;
 	private int port;
 	private int nodeNumber;
 	
 	public Node(String IP, int port, int nodeNumber){
-		this.IP = IP;
+		InetAddress ipholder;
+		try {
+			ipholder = InetAddress.getByName(IP);
+			this.IP = ipholder.getAddress();
+		} catch (UnknownHostException e) {
+			//everything explodes
+		}
 		this.port = port;
 		this.nodeNumber = nodeNumber;
 	}
 	
-	public String getIP(){
+	public byte[] getIP(){
 		return this.IP;
 	}
 	
