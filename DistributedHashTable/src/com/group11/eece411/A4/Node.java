3b7bf7d5a55f02cf5cefa16ca44a2f3ec1b80c93
Ryan Clarke <rclarke.ce@gmail.com>
2015-03-05 20:58:37 -0800

    did stuff

--- DistributedHashTable/src/com/group11/eece411/A4/Node.java
+++ DistributedHashTable/src/com/group11/eece411/A4/Node.java
@@ -7,31 +7,16 @@ import java.net.UnknownHostException;
  * Node data structure
  */
 public class Node {
-<<<<<<< HEAD
-	private byte[] IP;
-	private int port;
-	private int nodeNumber;
-	
-	public Node(String IP, int port, int nodeNumber){
-		InetAddress ipholder;
-		try {
-			ipholder = InetAddress.getByName(IP);
-			this.IP = ipholder.getAddress();
-		} catch (UnknownHostException e) {
-			//everything explodes
-		}
-		this.port = port;
-=======
+
 	private String IP;
 	private int nodeNumber;
 	
 	public Node(String IP, int nodeNumber){
 		this.IP = IP;
->>>>>>> origin/master
 		this.nodeNumber = nodeNumber;
 	}
 	
-	public byte[] getIP(){
+	public String getIP(){
 		return this.IP;
 	}
 	
