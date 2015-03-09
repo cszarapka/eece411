e73cf047d52993e0e6f5ebd843a50cf26b150e92
g17-sb <redhot_bouthot@hotmail.com>
2015-03-09 15:32:53 -0700

    formatted transfer.php

--- DistributedHashTable/transfer.php
+++ DistributedHashTable/transfer.php
@@ -1,11 +1,41 @@
-<?php $nodes = trim(explode('\n', file_get_contents("activeNodes.txt")))
+<?php 
+$nodes
+=
+explode(
+"\n"
+,
+file_get_contents(
+"activeNodes.txt"
+)
+)
 ;
-for($i = 0
+for(
+$i
+=
+0
 ;
-$i < count($nodes)
+$i
+<
+count(
+$nodes
+)
 ;
-$i++) {echo "scp Server.jar ubc_eece411_5@".$nodes[$i].":") 
+$i++
+)
+{
+echo
+shell_exec(
+"scp Server.jar ubc_eece411_5@"
+.$nodes[$i]
+.":"
+)
+."Transferred to "
+.$i
+." of "
+.count(
+$nodes
+)
+."\n"
 ;
-} sleep(3)
-;
+}
 ?>
